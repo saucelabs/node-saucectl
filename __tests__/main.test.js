d72a5cd5a5e8ead2d4844d295bf985e86abb96cd
dpgraham <dpgraham.85@gmail.com>
2021-01-22 16:44:07 -0800

    update test

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -3,17 +3,25 @@ const main = require('../')
 const childProcess = require('child_process');
 const { EventEmitter } = require('events');
 
-test('should run the executeable', async () => {
-    childProcess.spawn.mockImplementation(() => {
-        const mockSpawnEventEmitter = new EventEmitter();
-        mockSpawnEventEmitter.on("exit", () => {})
-        return mockSpawnEventEmitter;
-    });
-	const bin = {
-		run: jest.fn().mockReturnValue(Promise.resolve(true)),
-		path: jest.fn().mockReturnValue('/bin/saucectl')
-	}
-	await main(bin, ['bar', '--foo'])
-	expect(bin.run).toBeCalledTimes(1)
-	expect(childProcess.spawn).toBeCalledWith('/bin/saucectl', ['bar', '--foo'], expect.any(Object))
-})
+describe('main', function () {
+	let exitSpy, mockSpawnEventEmitter;
+	beforeAll(function () {
+		childProcess.spawn.mockImplementation(() => {
+			mockSpawnEventEmitter = new EventEmitter();
+			return mockSpawnEventEmitter;
+		});
+		exitSpy = jest.spyOn(process, 'exit');
+		exitSpy.mockImplementation(() => {});
+	});
+	test('should run the executeable', async () => {
+		const bin = {
+			run: jest.fn().mockReturnValue(Promise.resolve(true)),
+			path: jest.fn().mockReturnValue('/bin/saucectl')
+		}
+		await main(bin, ['bar', '--foo'])
+		expect(bin.run).toBeCalledTimes(1)
+		expect(childProcess.spawn).toBeCalledWith('/bin/saucectl', ['bar', '--foo'], expect.any(Object))
+		mockSpawnEventEmitter.emit('exit', 0);
+		expect(exitSpy.mock.calls).toEqual([[0]]);
+	})
+});

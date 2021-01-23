5d699c293eaab61289a3145d65e895dedd8c6820
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-01-22 16:08:51 -0800

    fix unit test

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,15 +1,19 @@
-const { spawn } = require('child_process')
-
-const main = require('../')
-
 jest.mock('child_process')
+const main = require('../')
+const childProcess = require('child_process');
+const { EventEmitter } = require('events');
 
 test('should run the executeable', async () => {
+    childProcess.spawn.mockImplementation(() => {
+        const mockSpawnEventEmitter = new EventEmitter();
+        mockSpawnEventEmitter.on("exit", () => {})
+        return mockSpawnEventEmitter;
+    });
 	const bin = {
 		run: jest.fn().mockReturnValue(Promise.resolve(true)),
 		path: jest.fn().mockReturnValue('/bin/saucectl')
 	}
 	await main(bin, ['bar', '--foo'])
 	expect(bin.run).toBeCalledTimes(1)
-	expect(spawn).toBeCalledWith('/bin/saucectl', ['bar', '--foo'], expect.any(Object))
+	expect(childProcess.spawn).toBeCalledWith('/bin/saucectl', ['bar', '--foo'], expect.any(Object))
 })

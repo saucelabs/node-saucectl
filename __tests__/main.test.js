9635a3574228363ed2c5e17b70a0fcfe57fc25fa
Alex Plischke <alex.plischke@saucelabs.com>
2023-10-20 13:24:24 -0700

    style: format all code with prettier (#27)
    * style: format all code with prettier
    
    * build: use more explicit test steps
    
    * ci: remove redundant job
    
    * ci: rename extremely confusing job name
    
    * ci: don't cache, since there's no lock file

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,4 +1,4 @@
-jest.mock('child_process')
+jest.mock('child_process');
 const main = require('../');
 const childProcess = require('child_process');
 const { EventEmitter } = require('events');
@@ -13,87 +13,101 @@ const originalOs = process.platform;
 const originalArch = process.arch;
 
 describe('main', function () {
-	let exitSpy, mockSpawnEventEmitter;
-	beforeAll(function () {
-		childProcess.spawn.mockImplementation(() => {
-			mockSpawnEventEmitter = new EventEmitter();
-			return mockSpawnEventEmitter;
-		});
-		exitSpy = jest.spyOn(process, 'exit');
-		exitSpy.mockImplementation(() => { });
-	});
+  let exitSpy, mockSpawnEventEmitter;
+  beforeAll(function () {
+    childProcess.spawn.mockImplementation(() => {
+      mockSpawnEventEmitter = new EventEmitter();
+      return mockSpawnEventEmitter;
+    });
+    exitSpy = jest.spyOn(process, 'exit');
+    exitSpy.mockImplementation(() => {});
+  });
 
-	beforeEach(() => {
-		BinWrapper.mockClear();
+  beforeEach(() => {
+    BinWrapper.mockClear();
 
-		Object.defineProperty(process, 'platform', { value: 'darwin' });
-		Object.defineProperty(process, 'arch', { value: 'arm64' });
+    Object.defineProperty(process, 'platform', { value: 'darwin' });
+    Object.defineProperty(process, 'arch', { value: 'arm64' });
 
-		mockSrc = jest.fn();
+    mockSrc = jest.fn();
 
-		BinWrapper.mockReturnValue({
-			use: jest.fn(),
-			dest: jest.fn(),
-			src: mockSrc,
-			run: jest.fn(),
-		});
-	});
-	afterEach(() => {
-		jest.clearAllMocks();
+    BinWrapper.mockReturnValue({
+      use: jest.fn(),
+      dest: jest.fn(),
+      src: mockSrc,
+      run: jest.fn(),
+    });
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
 
-		Object.defineProperty(process, 'platform', { value: originalOs });
-		Object.defineProperty(process, 'arch', { value: originalArch });
-	});
+    Object.defineProperty(process, 'platform', { value: originalOs });
+    Object.defineProperty(process, 'arch', { value: originalArch });
+  });
 
-	test('should run the executable', async function () {
-		const bin = {
-			run: jest.fn().mockReturnValue(Promise.resolve(0)),
-			path: jest.fn().mockReturnValue('/bin/saucectl')
-		}
-		await main(bin, ['bar', '--foo'])
-		expect(bin.run).toHaveBeenCalledTimes(1)
-		expect(childProcess.spawn).toHaveBeenCalledWith('/bin/saucectl', ['bar', '--foo'], expect.any(Object))
-		mockSpawnEventEmitter.emit('exit', 0);
-		expect(exitSpy.mock.calls).toEqual([[0]]);
-	});
-	test('should display carry the exitCode', async function () {
-		const bin = {
-			run: jest.fn().mockReturnValue(Promise.resolve(1)),
-			path: jest.fn().mockReturnValue('/bin/saucectl')
-		}
-		await main(bin, ['bar', '--foo'])
-		expect(bin.run).toHaveBeenCalledTimes(1)
-		expect(childProcess.spawn).toHaveBeenCalledWith('/bin/saucectl', ['bar', '--foo'], expect.any(Object))
-		expect(exitSpy.mock.calls).toEqual([[1]]);
-	});
-	describe('.binWrapper', function () {
-		test('should create a binary wrapper', async function () {
-			await main.binWrapper();
+  test('should run the executable', async function () {
+    const bin = {
+      run: jest.fn().mockReturnValue(Promise.resolve(0)),
+      path: jest.fn().mockReturnValue('/bin/saucectl'),
+    };
+    await main(bin, ['bar', '--foo']);
+    expect(bin.run).toHaveBeenCalledTimes(1);
+    expect(childProcess.spawn).toHaveBeenCalledWith(
+      '/bin/saucectl',
+      ['bar', '--foo'],
+      expect.any(Object),
+    );
+    mockSpawnEventEmitter.emit('exit', 0);
+    expect(exitSpy.mock.calls).toEqual([[0]]);
+  });
+  test('should display carry the exitCode', async function () {
+    const bin = {
+      run: jest.fn().mockReturnValue(Promise.resolve(1)),
+      path: jest.fn().mockReturnValue('/bin/saucectl'),
+    };
+    await main(bin, ['bar', '--foo']);
+    expect(bin.run).toHaveBeenCalledTimes(1);
+    expect(childProcess.spawn).toHaveBeenCalledWith(
+      '/bin/saucectl',
+      ['bar', '--foo'],
+      expect.any(Object),
+    );
+    expect(exitSpy.mock.calls).toEqual([[1]]);
+  });
+  describe('.binWrapper', function () {
+    test('should create a binary wrapper', async function () {
+      await main.binWrapper();
 
-			expect(mockSrc.mock.calls).toEqual([
-				[
-					'https://github.com/saucelabs/saucectl/releases/download/v{{version}}/saucectl_{{version}}_mac_arm64.tar.gz'.replaceAll('{{version}}', packageJson.version),
-					'darwin',
-					'arm64',
-				]
-			]);
-		});
-		test('should respect the SAUCECTL_INSTALL_BINARY wrapper', async function () {
-			await main.binWrapper('http://some-fake-url');
+      expect(mockSrc.mock.calls).toEqual([
+        [
+          'https://github.com/saucelabs/saucectl/releases/download/v{{version}}/saucectl_{{version}}_mac_arm64.tar.gz'.replaceAll(
+            '{{version}}',
+            packageJson.version,
+          ),
+          'darwin',
+          'arm64',
+        ],
+      ]);
+    });
+    test('should respect the SAUCECTL_INSTALL_BINARY wrapper', async function () {
+      await main.binWrapper('http://some-fake-url');
 
-			expect(mockSrc).toMatchSnapshot();
-		});
+      expect(mockSrc).toMatchSnapshot();
+    });
 
-		test('should respect the SAUCECTL_INSTALL_BINARY_MIRROR wrapper', async function () {
-			await main.binWrapper(null, 'https://some-fake-mirror-url');
+    test('should respect the SAUCECTL_INSTALL_BINARY_MIRROR wrapper', async function () {
+      await main.binWrapper(null, 'https://some-fake-mirror-url');
 
-			expect(mockSrc.mock.calls).toEqual([
-				[
-					'https://some-fake-mirror-url/v{{version}}/saucectl_{{version}}_mac_arm64.tar.gz'.replaceAll('{{version}}', packageJson.version),
-					'darwin',
-					'arm64',
-				]
-			]);
-		});
-	});
+      expect(mockSrc.mock.calls).toEqual([
+        [
+          'https://some-fake-mirror-url/v{{version}}/saucectl_{{version}}_mac_arm64.tar.gz'.replaceAll(
+            '{{version}}',
+            packageJson.version,
+          ),
+          'darwin',
+          'arm64',
+        ],
+      ]);
+    });
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    testEnvironment: 'node',
-    collectCoverage: true,
-    coverageThreshold: {
-		global: {
-			branches: 66,
-			functions: 75,
-			lines: 70,
-			statements: 70
-		}
-  }
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 66,
+      functions: 75,
+      lines: 70,
+      statements: 70,
+    },
+  },
 };
